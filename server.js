e664a4402b7b3e4ddc44ec0679b15e58567651d6
chairielazizi <azizihr2000@gmail.com>
2021-02-22 22:22:05 +0800

    setup the mongodb and the shorturl endpoint

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/urlShortener", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //set our views to use the index.ejs
 app.set("view engine", "ejs");
@@ -8,5 +14,8 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+//create endpoint
+app.post("/shortURL", (req, res) => {});
 
 app.listen(process.env.PORT || 5000);
